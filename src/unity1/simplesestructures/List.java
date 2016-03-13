844de2d528dae900990a4ea10872325fd444d50f
LEYDY <LEYDY@DESKTOP-VNFL2QQ>
2016-03-13 00:23:21 -0500

    ROCIO

--- src/unity1/simplesestructures/List.java
+++ src/unity1/simplesestructures/List.java
@@ -40,7 +40,7 @@ public class List {
         System.out.println  ("   ");
         
         }
-    public int count (){
+    public int Count (){
         Node aux= head;
         int counter =0;
         while (aux!=null){
@@ -101,7 +101,7 @@ public class List {
         while(aux.next!=null){
             aux=aux.next;
         }
-        int c=count();
+        int c=Count();
         for(int i=0;i<c;i++){
             Node n= new Node(p.data);
             aux.next=n;
@@ -110,7 +110,88 @@ public class List {
             
             
         }
-    }   
+    } 
+     public void removeIntercaled(){
+       Node aux= head,p=head;
+        int c=Count();
+        for(int i=0;i<c;i++){
+            if(i%2==1){
+                p.next=aux.next;
+            }
+            p=aux;
+            aux=aux.next;
+        }
+    }
+    public void DuplicateIntercaled(){
+        Node aux= head,p=head;
+        int c=Count();
+        while (aux.next != null){
+            Node n=new Node(aux.data);
+            n.next=aux.next;
+            aux.next=n;
+            aux=n.next;
+        }
+    }
+    public void Invert(){
+        Node aux= head,head2=null;
+        while(head!=null){
+            aux=head;
+            head=head.next;
+            aux.next=head2.next;
+            head2.next=aux;
+        }
+        head=head2;
+    }
+    public void RemoveRecurrents(){
+        Node p =head, aux=head, back=head;
+        while(aux.next!=null){
+            p=head;
+            while(p!=aux){
+                if(p.data==aux.data){
+                    back.next=aux.next;
+                    back=aux;
+                }
+                p=p.next;
+            }
+            back=aux;
+            aux=aux.next;
+        }
+    }
+    
+    public void InsertZeros(){
+        Node aux=head;
+        int  c=Count();
+        for(int i=0;i<c-1;i++){
+            Node n=new Node(0);
+            n.next=aux.next;
+            aux.next=n;
+            aux=n.next;
+        }
+    }
+    
+    public void removeMiddle(){
+        Node aux= head , back =head;
+        int c=Count();
+        int half=c/2;
+        
+         for(int i=0;i<half;i++){
+             back=aux;
+             aux=aux.next;
+             
+         }
+         back.next=aux.next;   
+    }
+    public int countOdds(){
+        Node aux= head; 
+        int cont=0;
+        while(aux.next!=null){
+            if(aux.data%2!=0){
+                cont++;
+            }
+            aux=aux.next;
+        }
+        return cont;
+    }
     }
 
 
