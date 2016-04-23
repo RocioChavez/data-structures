d021370aba6b7a1fd8d24981ac3984ab46ba0524
Acer <Acer@DESKTOP-8UI8GUS>
2016-04-23 16:59:41 -0500

    circular list and double circurlar list

---
+++ src/unity1/simplesestructures/circulardoubles/List.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity1.simplesestructures.circulardoubles;
+
+/**
+ *
+ * @author Acer
+ */
+public class List {
+        
+    Node head;
+    public List(){
+            head=null;
+    }
+    public void add(int d){
+      Node n=new Node(d);
+        if(head==null){
+        head=n;   
+       } else {
+        Node aux=head.back;
+       
+       
+        n.next=head;
+        n.back=aux;
+        head.back=n;
+         aux.next=n;
+        }
+    }
+    public void print(){
+        if(head==null) return;
+        Node aux=head;
+        do{
+            System.out.println(aux.data);
+            aux=aux.next; 
+        }while(aux!=head);
+    }
+}
---
+++ src/unity1/simplesestructures/circulardoubles/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity1.simplesestructures.circulardoubles;
+
+/**
+ *
+ * @author Acer
+ */
+public class Test {
+    public static void main(String Args[]){
+        List l=new List();
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        l.add(8);
+        l.print();
+    }
+}
